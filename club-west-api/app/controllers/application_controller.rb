28dcfa2fff0bdfff551e16b7c279365c9bd9319d
malissab <malissambishop7@gmail.com>
2022-02-08 12:20:37 -0800

    pushing updates

--- club-west-api/app/controllers/application_controller.rb
+++ club-west-api/app/controllers/application_controller.rb
@@ -4,5 +4,9 @@ class ApplicationController < ActionController::API
     def current_user
         User.find_by(id: session[:user_id])
     end
+
+    def home
+        render json: {message: 'Server is running!'}
+    end
     
 end
